c5a6414a0921e8fb9b029c9f1333529aa2454af3
dan <danielkarongoi@gmail.com>
2025-03-10 16:39:22 +0300

    added changes

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 function writeCards(names, event) {
-    let messages = []; 
-    for (let i = 0; i < names.length; i++) {
-      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
-    }
-    return messages;
+  let messages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
   }
-  
-  function countDown(number) {
-    while (number >= 0) {
-      console.log(number);
-      number--;
-    }
+
+  return messages;
+}
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
   }
-  
-  // Test the functions
-  console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
-  countDown(5);
-  
+}
+
+// Example usage
+const names = ["Charlie", "Tom", "George", "Njogu"];
+const event = "birthday";
+
+console.log(writeCards(names, event));
+countDown(10);
